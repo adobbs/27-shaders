12b9afe7cfe142be8c0082a46d24c51540356c90
Andy Dobbs <andrew.dobbs@gmail.com>
2023-03-28 16:43:02 -0400

    Create a wave effect using vertex shader

--- src/script.js
+++ src/script.js
@@ -45,8 +45,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -86,8 +85,7 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
